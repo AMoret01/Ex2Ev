b9c447f67c50fcc7fca39cca3aee448b44fa4add
AMoret01 <amoretgomez@gmail.com>
2021-05-10 20:31:52 +0200

    Ejercicio 6 sin finalizar

--- src/App.js
+++ src/App.js
@@ -2,45 +2,24 @@ import {EXButton} from './EXButton/EXButton';
 import {JustDoIt} from './EXButton/EXButton';
 import './App.css';
 import Inside from './Inside';
-import {Switch, Route, Link} from 'react-router-dom';
-import EXSecondView from './EXSecondView'
-import { Fragment } from 'react';
 
-//const message = ["Here button. I called you!"];
+const message = ["Here button. I called you!"];
 
-// const callMe = (props) => {
-//   if (props === message)
-//   {
-//     function finalMessage(){
-//       const result = message.map((item,index) => <EXButton key={index} title = {item}></EXButton>)
-//       return result;
-//     }
-//     return finalMessage()
-//   }
-// };
-function App() {
-  return (
-    <Fragment>
-    <div className="main-screen">
-        <Inside TITULO = {'Already in!'}>Welcome to the IT world!</Inside>
-        <EXButton /*method={callMe(message)}*/ title={"Listo para hacer clic"}></EXButton>
-        <h2>
-          <Link to={'/EXSecondView'}>
-            Ir a EXSecondView
-          </Link>
-        </h2>
-    </div>
-    <div>
-      <Switch>
-          <Route path={'/Home'}>
-          </Route>
-          <Route path={'/EXSecondView'}>
-            <EXSecondView/>
-          </Route>
-        </Switch>
-    </div>
-    </Fragment>
-  )
-} 
+const callMe = (props) => {
+   if (props === message)   {
+        function finalMessage(){
+        const result = message.map((item,index) => <EXButton key={index} title = {item}></EXButton>)
+        return result;
+      }
+      return finalMessage();
+   }
+};
+
+const App = () => (
+  <div className="main-screen">
+    <Inside TITULO = {'Already in!'}>Welcome to the IT world!</Inside>
+    <EXButton method={callMe(message)} title={"Listo para hacer clic"}></EXButton>
+  </div>
+);
 JustDoIt();
 export default App;
